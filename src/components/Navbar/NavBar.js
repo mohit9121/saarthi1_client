48d422bc56c7f7e3d6556ebb4c8b3554919b79c4
tushxr16 <2020meb1325@iitrpr.ac.in>
2023-03-30 00:03:31 +0530

    a little bit of this and a little bit of that

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,67 +1,71 @@
-// Navbar.js
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-// import logo from '../../assets/logo192.png';
-import logo from '../../assets/logosar1.png';
-import './NavBar.css';
-import axios from 'axios';
-import Login from '../Login/Login.js';
-import { useNavigate } from 'react-router-dom';
-
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
+import "./NavBar.css";
+import axios from "axios";
 
 const Navbar = () => {
+  
   const id = localStorage.getItem("token");
-  // console.log("id he "+id);
-  const [users, setUser] = useState([]);
+  const [user, setUser] = useState();
+
   useEffect(() => {
     (async () => {
       try {
-        const response = await axios.get(`http://localhost:8082/api/users/${id}`);
-        setUser(response.data);
-      } catch (err) {
-        console.error(err);
-      }
+        if (id != null) {
+          const response = await axios.get(
+            `http://localhost:8082/api/users/${id}`
+          );
+          if (response.status === 200) setUser(response.data);
+        }
+      } catch (err) {}
     })();
   }, [id]);
-  const userName = users.username; 
 
-  const navigate = useNavigate();
   const handleLogout = () => {
-    // Remove the JWT from local storage
-    console.log("andar gaya ");
     localStorage.removeItem("token");
     window.location.reload();
-    // navigate('/test')
   };
 
   return (
-    <nav className="navbar">
+    <nav className="navbar_">
       <div className="navbar__logo">
-        <img src={logo} alt="Logo" className="navbar__logo-img"/>
+        <img src={logo} alt="Logo" className="navbar__logo-img" />
       </div>
       <ul className="navbar__menu">
         <li>
-          <Link to="/test" className="navbar__menu-item">Home</Link>
+          <Link to="/test" className="navbar__menu-item">
+            Home
+          </Link>
         </li>
         <li>
-          <Link to="/about" className="navbar__menu-item">About</Link>
+          <Link to="/about" className="navbar__menu-item">
+            About
+          </Link>
         </li>
         <li>
-          <Link to="/show-items" className="navbar__menu-item">Your Products</Link>
+          <Link to="/show-items" className="navbar__menu-item">
+            Your Products
+          </Link>
         </li>
         <li>
-          <Link to="/discussion-forum" className="navbar__menu-item">Discussion</Link>
+          <Link to="/discussion-forum" className="navbar__menu-item">
+            Discussion
+          </Link>
         </li>
         <li>
-          <Link to="/contact" className="navbar__menu-item">Contact</Link>
+          <Link to="/contact" className="navbar__menu-item">
+            Contact
+          </Link>
         </li>
       </ul>
       <div className="navbar__user">
-        {userName}
-        {userName?(
-          <button className="header__logout-button" onClick={handleLogout}>Logout</button>
-        ):(
-          <Link to ='/login'>Login</Link>
+        {user ? (
+          <button className="btn btn-danger-info" onClick={handleLogout}>
+            Logout
+          </button>
+        ) : (
+          <Link to="/login" className="text-light btn btn-outline-info">Login</Link>
         )}
       </div>
     </nav>
--- src/components/ShowProducts.js
+++ src/components/ShowProducts.js
@@ -1,116 +1,61 @@
-import React, { useState, useEffect } from 'react';
-import '../App.css';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import BookCard from './ProductCard.js';
-import { useNavigate } from 'react-router-dom';
-// import jwt from "jsonwebtoken";
+import React, { useState, useEffect } from "react";
+import "../App.css";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import BookCard from "./ProductCard.js";
+import { useNavigate } from "react-router-dom";
+import Navbar from "./Navbar/NavBar.js";
 
 function ShowBookList() {
 
-  const navigate = useNavigate();
-  const token = localStorage.getItem("token");
-  // console.log(token); 
-  const id = token;
-  const handleLogout = () => {
-    // Remove the JWT from local storage
-    console.log("andar gaya ");
-    localStorage.removeItem("token");
-
-    // Navigate to the login page
-    // props.history.push("/login");
-    navigate('/')
-  };
-  // const payload = jwt.decode(token);
-  // console.log(payload);
-  
   const [books, setBooks] = useState([]);
 
   useEffect(() => {
     axios
-      .get('http://localhost:8082/api/products')
+      .get("http://localhost:8082/api/products")
       .then((res) => {
         setBooks(res.data);
       })
       .catch((err) => {
-        console.log('Error from ShowBookList');
+        console.log("Error from ShowBookList");
       });
   }, []);
 
-
-  // console.log("no user"); 
-  const [users, setUser] = useState([]);
-  useEffect(() => {
-    (async () => {
-      try {
-        const response = await axios.get(`http://localhost:8082/api/users/${id}`);
-        setUser(response.data);
-      } catch (err) {
-        console.error(err);
-      }
-    })();
-  }, [id]);
-  // console.log(users)
-
-
-
   const bookList =
     books.length === 0
-      ? 'You Havent added any product till now '
+      ? "You Haven't added any product till now "
       : books.map((book, k) => <BookCard book={book} key={k} />);
 
   return (
-    <div className='ShowBookList'>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-md-12'>
+    <div className="ShowBookList">
+      <div className="container">
+        <>
+          <Navbar />
+        </>
+        <div className="row">
+          <div className="col-md-12">
             <br />
-            <h2 className='display-4 text-center'>Current Product List</h2>
+            <h2 className="display-4 text-center">Current Product List</h2>
           </div>
 
-          <div className='col-md-11'>
+          <div className="col-md-11">
             <Link
-              to='/create-book'
-              className='btn btn-outline-warning float-right'
-            >
-              + Add New product
-            </Link>
-            <Link
-              to='/discussion-forum'
-              className='btn btn-outline-warning float-right'
+              to="/discussion-forum"
+              className="btn btn-outline-warning float-right m-1"
             >
               Discussion forum
             </Link>
-            <br />
-            <br />
-            <hr />
+            <Link
+              to="/create-book"
+              className="btn btn-outline-warning float-right m-1"
+            >
+              + Add New product
+            </Link>
           </div>
         </div>
 
-        <div className='list'>{bookList}</div>
+        <div className="list">{bookList}</div>
       </div>
-      <div>
-        {id ? (
-          <>
-            <h1>Welcome, {users.username}!</h1>
-            <button onClick={handleLogout}>Logout</button>
-          </>
-        ) : (
-          <>
-            <h1>Welcome</h1>
-            <Link
-              to='/login'
-              className='btn btn-outline-warning float-right'
-            >
-              Login
-            </Link>
-          </>
-        )}
-        {/* <LogoutButton {...props} /> */}
-      </div>
-      {/* <div className='info1'>
-        <h1>welcome {users.username}</h1>
-      </div> */}
     </div>
   );
 }
